7a19686506a36713c589925d02bb389a62fb45af
Merciless-S <2051183268yjz@gmail.com>
2019-12-24 12:24:26 -0500

    icecream added

--- snake.js
+++ snake.js
@@ -5,27 +5,36 @@ const context = canvas.getContext('2d');
 
 
 //body:1, food:2
-context.scale(20,20);
+context.scale(10,10);
 
-const color = ['red', 'green', 'blue'];
+const color = ['black', 'green', 'blue', 'yellow'];
 const player = {
     arena: null,
     deque: null,
     direction: null,
-    score: 0
+    score: 0,
+    ice_cream: 0,
+    ice_creamPos:null
 }
 
 let dropCounter = 0;
-let dropInterval = 80;
+let dropInterval = 50;
 let lastTime = 0;
 
 function update(time = 0){
     const deltaTime = time - lastTime;
-
     dropCounter += deltaTime;
     if(dropCounter > dropInterval){
         makeMove();
         dropCounter = 0;
+    }
+    if(player.ice_cream > 0){
+        player.ice_cream =  Math.floor(player.ice_cream - 0.01 * deltaTime);
+        if(player.ice_cream <= 0){
+            player.ice_cream = 0;
+            player.arena[player.ice_creamPos[0]][player.ice_creamPos[1]] = 0;
+            player.ice_creamPos = null;
+        }
     }
     lastTime = time;
     drawArena();
@@ -33,7 +42,6 @@ function update(time = 0){
 }
 
 function makeMove(){
-    //console.log(player.direction)
     let rl = player.arena.length;
     let cl = player.arena[0].length;
     let head = player.deque[player.deque.length - 1];
@@ -47,7 +55,7 @@ function makeMove(){
     }else if(newHead[1] < 0){
         newHead[1] = cl - 1;
     }
-    if(player.arena[newHead[0]][newHead[1]] != 2){
+    if(player.arena[newHead[0]][newHead[1]] != 2 && player.arena[newHead[0]][newHead[1]] != 3){
         let tail = player.deque.shift();
         player.arena[tail[0]][tail[1]] = 0;
     }
@@ -58,17 +66,24 @@ function makeMove(){
         updateScore();
     }else if(player.arena[newHead[0]][newHead[1]] == 2){
         generateRandomFood();
-        player.score += 5;
+        player.score += 100;
         updateScore();
+    }else if(player.arena[newHead[0]][newHead[1]] == 3){
+        player.score += player.ice_cream
+        player.ice_cream = 0;
+        player.ice_creamPos = null;
+        updateScore()
     }
     player.deque.push(newHead);
     player.arena[newHead[0]][newHead[1]] = 1;
 }
 
 function resetGame(){
-    createMatrix(20,20);
+    createMatrix(40,40);
     initializePlayer();
     player.score = 0;
+    player.ice_cream = 0;
+    player.ice_creamPos = null
     if(player.deque[0][1] > 8){
         player.deque.unshift([player.deque[0][0],player.deque[0][1] - 1 ])
         player.direction = [0,1];
@@ -106,9 +121,24 @@ function generateRandomFood(){
             }
         }
     }
-    [x, y] = temp[Math.floor(Math.random() * temp.length)]
-    //[x, y] = [Math.floor(Math.random() * rl),Math.floor(Math.random() * cl)];
+    if(temp.length == 0) {
+        alert(`You Win!\n You score is ${player.score}`);
+        resetGame();
+        update();
+        updateScore();
+        return;
+    }
+    let food = Math.floor(Math.random() * temp.length);
+    let [x, y] = temp[food];
     player.arena[x][y] = 2;
+    if(temp.length > 1 && Math.random() < 0.2 && player.ice_creamPos == null){
+        temp.slice(food, 1);
+        [x, y] = temp[Math.floor(Math.random() * temp.length)];
+        player.arena[x][y] = 3;
+        player.ice_cream = 300;
+        player.ice_creamPos = [x,y];
+    }
+    //[x, y] = [Math.floor(Math.random() * rl),Math.floor(Math.random() * cl)];
 }
 
 function drawArena(){
@@ -118,18 +148,24 @@ function drawArena(){
     let cl = player.arena[0].length;
     for(let i = 0; i < rl; i++){
         for(let j = 0; j < cl; j++){
-            if(player.arena[i][j] !== 0){
-                    //console.log(i, j);
+
                 context.fillStyle = color[player.arena[i][j]];
                 context.fillRect(j, i,1,1);
-            }
+
         }
     }
+    updateIce_cream();
 }
 
+function updateIce_cream(){
+    if(player.ice_creamPos == null){
+        document.getElementById('ice_cream').innerText = null;
+    }else
+        document.getElementById('ice_cream').innerText = `Ice-Cream: ${player.ice_cream}`;
+}
+
+
 function playerMove(x,y){
-
-
     let rl = player.arena.length;
     let cl = player.arena[0].length;
     let cur = player.deque[player.deque.length - 1];
@@ -160,20 +196,12 @@ function playerMove(x,y){
 document.addEventListener('keydown', event =>{
     if (event.keyCode == 40){
         playerMove(1,0);
-        //if(player.direction[0] != -1 || player.direction[1] != 0 )
-            //player.direction = [1,0];
     }else if(event.keyCode == 39){
         playerMove(0,1);
-        //if(player.direction[0] != 0 || player.direction[1] != -1)
-            //player.direction = [0,1];
     }else if(event.keyCode == 37){
         playerMove(0,-1);
-        //if(player.direction[0] != 0 || player.direction[1] != 1)
-            //player.direction = [0,-1];
     }else if(event.keyCode == 38){
         playerMove(-1,0);
-        //if(player.direction[0] != 1 || player.direction[1] != 0)
-            //player.direction = [-1,0];
     }
 });
 
